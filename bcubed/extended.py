0df3ad528cc6a0170afb9fb643d101e1e13d5a75
Hugo Hromic <hugo.hromic@insight-centre.org>
2018-12-12 15:58:55 +0000

    updated license

--- bcubed/extended.py
+++ bcubed/extended.py
@@ -1,9 +1,21 @@
-# Hugo Hromic - http://github.com/hhromic
+# Simple extended BCubed implementation in Python for clustering evaluation
+# Copyright 2015 Hugo Hromic
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 #
 # Extended BCubed algorithm taken from:
-# Amigo, Enrique, et al. "A comparison of extrinsic clustering evaluation
-# metrics based on formal constraints."
-# Information retrieval 12.4 (2009): 461-486.
+# Amigo, Enrique, et al. "A comparison of extrinsic clustering evaluation metrics
+# based on formal constraints." Information retrieval 12.4 (2009): 461-486.
 
 """Generate extended BCubed evaluation for clustering."""
 
